6e3fccd79f3d87ea2977fb295b7d28de0698e0f6
andreifba <89176882+andreifba@users.noreply.github.com>
2022-08-28 16:56:21 -0300

    Update q05.c

--- listafuncao/q05.c
+++ listafuncao/q05.c
@@ -1,44 +1,38 @@
 # include <stdio.h>
 
 /*
-Crie um programa que tenha uma função ler4Numeros. Essa função deve ler quatro
-números do usuario e retornar os quatro números. A função main deve imprimir esses quatro números. Utilize struct para fazer o retorno.
+Crie um programa que tenha uma função ler4Numeros.
+Essa função deve ler quatro números do usuario
+e retornar os quatro números. A função main deve
+imprimir esses quatro números. Utilize struct para fazer o retorno.
 */
 
 typedef struct
   {
-  int a;
-  int b;
-  int c;
-  int d;
+  int a, b, c, d;
+
   }numeros;
 
-ler4Numeros( int numeros);
+numeros ler4Numeros(){
+  numeros n;
+  printf("Digite o primeiro numero:");
+  scanf("%d",&n.a);
+  printf("Digite o segundo numero:");
+  scanf("%d",&n.b);
+  printf("Digite o terceiro numero:");
+  scanf("%d",&n.c);
+  printf("Digite o quarto numero:");
+  scanf("%d",&n.d);
+  return n;
+}
 
 int main ()
   {
-  
+    numeros n;
 
-
-  
+    n =ler4Numeros();
+      printf("%d%d%d%d",n.a,n.b,n.c,n.d);
 
      return 0;
   }
-  ler4Numeros( int numeros)
-  {
-    numeros numeros;
-
-    printf("Digite os valores: ");
-    scanf("%d",&numeros.a);
-    printf("Digite os valores: ");
-    scanf("%d",&numeros.b);
-    printf("Digite os valores: ");
-    scanf("%d",&numeros.c);
-    printf("Digite os valores: ");
-    scanf("%d",&numeros.b);
-
-    
-    
-  }
-
 
